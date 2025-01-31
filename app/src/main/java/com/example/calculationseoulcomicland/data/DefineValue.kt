6f3d263f8857dfe9a3023f3bfcee4c26db51a701
SeoFlu <lunarite0529@gmail.com>
2025-02-01 01:10:46 +0900

    add preferenceValue

--- app/src/main/java/com/example/calculationseoulcomicland/data/DefineValue.kt
+++ app/src/main/java/com/example/calculationseoulcomicland/data/DefineValue.kt
@@ -1,5 +1,16 @@
 package com.example.calculationseoulcomicland.data
 
 object DefineValue {
+    // fragment state
+    const val PREFERENCE_KEY_FRAGMENT_TAG = "fragment_tag"
+
+    const val PREFERENCE_KEY_FRAGMENT_TAG_HOME = "fragment_tag_home"
+    const val PREFERENCE_KEY_FRAGMENT_TAG_CALCULATE = "fragment_tag_calculate"
+    const val PREFERENCE_KEY_FRAGMENT_TAG_PROFILE = "fragment_tag_profile"
+    const val PREFERENCE_KEY_FRAGMENT_TAG_STOCK = "fragment_tag_stock"
+    const val PREFERENCE_KEY_FRAGMENT_TAG_TOTAL = "fragment_tag_total"
+
+    // value
     const val PREFERENCE_KEY_STOCK_INFO_STRING = "stock_info_string"
+    const val PREFERENCE_KEY_PROFILE_INFO_STRING = "profile_info_string"
 }
