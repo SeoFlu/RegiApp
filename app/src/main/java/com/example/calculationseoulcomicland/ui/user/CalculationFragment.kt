45fcf895210b3a527e439911f51a0aab1b24f97e
SeoFlu <lunarite0529@gmail.com>
2025-02-08 04:39:53 +0900

    apply init calculation fragment

--- app/src/main/java/com/example/calculationseoulcomicland/ui/user/CalculationFragment.kt
+++ app/src/main/java/com/example/calculationseoulcomicland/ui/user/CalculationFragment.kt
@@ -1,28 +1,33 @@
 package com.example.calculationseoulcomicland.ui.user
 
+import android.content.Context
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.calculationseoulcomicland.MainActivity
 import com.example.calculationseoulcomicland.MainActivity.Companion.preferences
-import com.example.calculationseoulcomicland.R
+import com.example.calculationseoulcomicland.data.CalItem
 import com.example.calculationseoulcomicland.data.DefineValue
 import com.example.calculationseoulcomicland.data.StockItem
+import com.example.calculationseoulcomicland.databinding.FragmentCalculationBinding
 import com.example.calculationseoulcomicland.ui.view.CalListAdapter
 import com.example.calculationseoulcomicland.util.DataUtil
 import com.example.calculationseoulcomicland.util.PreferenceUtil
 
 class CalculationFragment : Fragment() {
 
-    var itemList : ArrayList<StockItem> = ArrayList<StockItem>()
-    val calListAdapter = CalListAdapter(itemList)
+    private val LOG_TAG = "CalculationFragment."
+    private val CAL_ITEM_LIST_ITEM_WIDTH = 190
+
+    private var mBinding: FragmentCalculationBinding? = null
+    private val binding get() = mBinding!!
+    private val arrItems = mutableListOf<StockItem>()
+    private lateinit var calListAdapter: CalListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,30 +39,47 @@ class CalculationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         Log.d("TAG", "onCreateView: ")
-        val view : View = inflater.inflate(R.layout.fragment_calculation, container, false)
-
+        mBinding = FragmentCalculationBinding.inflate(inflater, container, false)
+        initView(binding)
         initPreference()
         initData()
 
-        initView(view)
-
-        return view
+        return binding.root
     }
 
-    private fun initView(view : View){
-        val calList : RecyclerView = view.findViewById(R.id.cal_recycler_view)
-        val calBtn : Button = view.findViewById(R.id.cal_success)
-        val calTotal : TextView = view.findViewById(R.id.cal_total)
+    private fun initView(view : FragmentCalculationBinding){
+        calListAdapter = CalListAdapter(context!!)
+        mBinding!!.calRecyclerView.layoutManager =
+            GridLayoutManager(context, getCountItemFromWidth())
+        calListAdapter.setItemClickListener(object :
+            CalListAdapter.OnItemClickListener {
+            override fun onClick(v: View, pos: Int) {
+                when (v.id) {
 
-        calListAdapter.notifyDataSetChanged()
+                }
+            }
+        })
 
-        calList.adapter = calListAdapter
-        calList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        mBinding!!.calRecyclerView.adapter = calListAdapter
 
+    }
 
-        calBtn.setOnClickListener{
-//            preferences.setString(DefineValue.PREFERENCE_KEY_STOCK_INFO_STRING, stockListAdapter.getListData())
-        }
+    private fun getCountItemFromWidth(): Int{
+        var itemCount = 1
+
+        val display = this.context?.resources?.displayMetrics
+        val deviceWidth = display?.widthPixels
+        itemCount = px2dp(deviceWidth!!, this.context!!).toInt() / CAL_ITEM_LIST_ITEM_WIDTH
+        Log.d(LOG_TAG, "getCountItemFromWidth: $itemCount $deviceWidth")
+
+        if(itemCount < 1)
+            return 1
+        else
+            return itemCount
+    }
+
+    fun px2dp(px: Int, context: Context): Float {
+        return px / ((context.resources.displayMetrics.densityDpi.toFloat()) / DisplayMetrics.DENSITY_DEFAULT)
     }
 
     private fun initPreference(){
@@ -68,9 +90,23 @@ class CalculationFragment : Fragment() {
 
     private fun initData() {
         Log.d("TAG", "initData: ")
-        var arrItems : ArrayList<StockItem> = DataUtil.StringToStockItemList(preferences.getString(DefineValue.PREFERENCE_KEY_STOCK_INFO_STRING, ""))
-        Log.d("TAG", "initData: "+arrItems.toString())
-        if(arrItems.isNotEmpty())
-            calListAdapter.updateList(arrItems)
+        resetListItem()
+    }
+
+    private fun resetListItem(){
+
+        arrItems.apply {
+            arrItems.clear()
+
+            val arrCalItems = DataUtil.StringToStockItemList( preferences.getString(
+                DefineValue.PREFERENCE_KEY_STOCK_INFO_STRING, ""))
+            for( item in arrCalItems ){
+                add(item)
+            }
+            calListAdapter.differ.submitList(arrItems)
+
+            Log.d(LOG_TAG, "resetListItem arrItems : "+arrItems)
+            calListAdapter.notifyDataSetChanged()
+        }
     }
 }
